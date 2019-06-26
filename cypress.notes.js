b8f675c2a23ee87d8c382747d2d525944cfde8ce
Tayte Stokes <taytestokes9328@gmail.com>
2019-06-25 20:26:12 -0600

    brought in cypress testing suite and created todo list

--- cypress.notes.js
+++ cypress.notes.js
@@ -5,4 +5,26 @@
 // first, install cypress into the project by running npm install cypress -D or yarn add cypress -D
 // second, hop into the package.json and add the script "cypress:open": "cypress open"
 // in the terminal, run "npm run cypress:open" or "yarn run cypress:open"
+// the first time we run this cypress will create a folder with it's config
+// this will open cypress suite to see our tests
 
+// create a 'sample_spec.js' file under cypress/integration
+// cypress will pick this up immediately and update the suite
+// thhe integration folder is where we can write our tests
+// Lets start writing those tests
+
+// The Syntax to Write this test looks very familiar to chai and mocha (or jest)
+describe('my first test', () => {
+    it('Does not do much', () => {
+        expect(true).to.equal(true)
+    })
+});
+
+// Tests generally follow a common structure using three phases
+// Arrange - Setup the application state
+    // visit a page
+    // query an element
+// Act - Take an action
+    // take an action on the element (click)
+// Assert - Make an assertion
+    // make an assertion of what to expect
--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // State
+  state = {
+    todos: ['workout', 'eat', 'code'],
+    userInput: ''
+  };
+
+  // Methods
+  removeTodo = index => {
+    const {todos} = this.state;
+    const updatedTodos = todos.filter((todo, i) => i !== index);
+    this.setState({
+      todos: updatedTodos
+    })
+  };
+
+  addTodo = () => {
+    const {userInput, todos} = this.state;
+    const updatedTodos = [...todos, userInput];
+    this.setState({
+      todos: updatedTodos,
+      userInput: ''
+    })
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      userInput: event.target.value
+    })
+  };
+
+  render() {
+    // map through the todos list
+    const mappedTodos = this.state.todos.map((todo, index) => (
+      <div>
+        <h1>{todo}</h1>
+        <button onClick={() => this.removeTodo(index)}>&#10003;</button>
+      </div>
+    ));
+
+
+    return (
+      <div>
+        <h1>Another Todo List... Yay</h1>
+        {mappedTodos}
+        <div>
+          <input type="text" onChange={this.handleInputChange} value={this.state.userInput}/>
+          <button onClick={this.addTodo}>&#43;</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
