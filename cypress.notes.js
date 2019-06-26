725ef28a3029f274d17638d2afe24b475e4c8a1e
Tayte Stokes <taytestokes9328@gmail.com>
2019-06-25 19:58:26 -0600

    initial setup for CRA

---
+++ cypress.notes.js
@@ -0,0 +1,8 @@
+// Cypress is a front end testing that will drives the browser the way a user would
+// Cypress will test the what of our application rather than the how
+
+// Get Started
+// first, install cypress into the project by running npm install cypress -D or yarn add cypress -D
+// second, hop into the package.json and add the script "cypress:open": "cypress open"
+// in the terminal, run "npm run cypress:open" or "yarn run cypress:open"
+
